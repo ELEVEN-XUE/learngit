c21d8d3dc77bd0fde8dc02962888645fadcf1356
xuexu <408062091@qq.com>
2021-12-07 11:36:55 +0800

    First Commit

---
+++ learngitDEMO/learngitDEMO/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  learngitDEMO
+//
+//  Created by 薛旭 on 2021/12/7.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
